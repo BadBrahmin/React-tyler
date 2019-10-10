6adf161c867170e9f84ae55106b1c080c1d4f316
BadBrahmin <tejasshree95@gmail.com>
2019-10-10 12:26:39 +0530

    add dark-light mode

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Popular from "./components/popular";
+import DarkMode from "./components/DarkMode";
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className="container">
+        {" "}
+        <DarkMode />{" "}
+      </div>
+    );
   }
 }
 
