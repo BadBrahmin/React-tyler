e9c6572cda00c1acf5c70fc6f24dcea5bda31a9b
BadBrahmin <tejasshree95@gmail.com>
2019-10-10 12:49:22 +0530

    destructured nav into fn component

--- github-battle/app/components/popular.js
+++ github-battle/app/components/popular.js
@@ -1,24 +1,51 @@
-import React from 'react'
+import React from "react";
 
-export default class Popular extends React.Component{
-    render () {
-
-        const languages = [ 'All', 'JavaScript', 'Ruby', 'Java', 'Python']
-        return (
-
-            <ul className="flex-center">
-                {languages.map ((language) => (
-                  <li key={language}>
-                      <button className='btn-clear nav-link'>
-                          {language}
-                      </button>
-
-                  </li>  
-                ) )}
-
-            </ul>
-           
-        )
-    }
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "Python"];
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={
+              language === selected ? { color: "tomato" } : { color: "blue" }
+            }
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
+export default class Popular extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedLanguage: "All"
+    };
+    console.log(this);
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+
+    return (
+      <>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </>
+    );
+  }
+}
--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/popular";
-import DarkMode from "./components/DarkMode";
+// import DarkMode from "./components/DarkMode";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         {" "}
-        <DarkMode />{" "}
+        <Popular />{" "}
       </div>
     );
   }
